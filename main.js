70da46521ca27ededffe031a3dd9bfb7e40efd80
sanju12327 <80778249+sanju12327@users.noreply.github.com>
2021-10-11 19:00:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized!');
+}
+
+function draw()
+{
+    background("#a9a9a9");
+    text("Hello World!!", noseX, noseY);
+    textSize(difference);
+    fill("#ffe787");
+    document.getElementById("hw").innerHTML = "Font Size is = " + difference + "px";
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log(" leftWristX = " + leftWristX + " rightWristX = " +rightWristX + " difference = " + difference);
+
+    }
+}
